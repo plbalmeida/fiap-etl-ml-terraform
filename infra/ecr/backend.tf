d24bb9e2a2b757d480a2c6e9d3442bc334f4f629
plbalmeida <plbalmeida@usp.br>
2024-12-06 18:33:11 -0300

    fix(infra/*): correção dos nomes dos buckets para armazenamento dos arquivos de estado do Terraform

--- infra/ecr/backend.tf
+++ infra/ecr/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     region         = "us-east-1"
     role_arn       = "arn:aws:iam::413467296690:role/ecr-ipea-eia366-pbrent366-terraform-backend-role"
-    bucket         = "ecr-ipea-eia366-pbrent366"
+    bucket         = "terraform-backend-ecr-ipea-eia366-pbrent366"
     key            = "terraform.tfstate"
     dynamodb_table = "terraform-backend-lock-table-ecr-ipea-eia366-pbrent366"
     encrypt        = true
--- infra/lambda/backend.tf
+++ infra/lambda/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     region         = "us-east-1"
     role_arn       = "arn:aws:iam::413467296690:role/lambda-ipea-eia366-pbrent366-terraform-backend-role"
-    bucket         = "lambda-ipea-eia366-pbrent366"
+    bucket         = "terraform-backend-lambda-ipea-eia366-pbrent366"
     key            = "terraform.tfstate"
     dynamodb_table = "terraform-backend-lock-table-lambda-ipea-eia366-pbrent366"
     encrypt        = true
