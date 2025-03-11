c08c0c7fb4ae23ca769adf8af2bf3b06784e06d8
plbalmeida <plbalmeida@usp.br>
2025-03-11 15:32:20 -0300

    fix(infra/terraform-backend/*): inclusão do ID da conta para possibilitar a criação das tabelas do DynamoDB

--- infra/terraform-backend/lambda/dynamodb/variables.tf
+++ infra/terraform-backend/lambda/dynamodb/variables.tf
@@ -1,5 +1,5 @@
 variable "component_name" {
   description = "Component name"
   type        = string
-  default     = "lambda-ipea-eia366-pbrent366"
+  default     = "lambda-ipea-eia366-pbrent366-626957573797"
 }
--- infra/terraform-backend/stepfunctions/s3/variables.tf
+++ infra/terraform-backend/stepfunctions/s3/variables.tf
@@ -1,5 +1,5 @@
 variable "component_name" {
   description = "Component name"
   type        = string
-  default     = "stepfunctions-ipea-eia366-pbrent366-626957573797"
+  default     = "sf-ipea-eia366-pbrent366-626957573797"
 }
