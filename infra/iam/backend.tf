cae6f6f05e020f3f04a9927a5d3d49be9d988bf4
plbalmeida <plbalmeida@usp.br>
2025-03-11 18:57:03 -0300

    chore(infra/iam/backend.tf): teste sem parâmetro de role

--- infra/iam/backend.tf
+++ infra/iam/backend.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "s3" {
     region         = "us-east-1"
-    role_arn       = "arn:aws:iam::413467296690:role/iam-ipea-eia366-pbrent366-terraform-backend-role"
     bucket         = "terraform-backend-iam-ipea-eia366-pbrent366"
     key            = "terraform.tfstate"
     dynamodb_table = "terraform-backend-lock-table-iam-ipea-eia366-pbrent366"
