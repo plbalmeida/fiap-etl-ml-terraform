708fddfdcc42a986bcb241086affb5a14f792026
plbalmeida <plbalmeida@usp.br>
2025-06-27 15:47:43 -0300

    fix(infra/*): correção da role para a role LabRole da AWS academy

--- infra/stepfunctions/backend.tf
+++ infra/stepfunctions/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     region         = "us-east-1"
-    role_arn       = "arn:aws:iam::413467296690:role/stepfunctions-ipea-eia366-pbrent366-terraform-backend-role"
+    role_arn       = "arn:aws:iam::626957573797:role/LabRole"
     bucket         = "terraform-backend-stepfunctions-ipea-eia366-pbrent366"
     key            = "terraform.tfstate"
     dynamodb_table = "terraform-backend-lock-table-stepfunctions-ipea-eia366-pbrent366"
