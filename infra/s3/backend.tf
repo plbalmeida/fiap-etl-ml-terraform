1b2f05e176edd9fb8f11bc57232b303c48ff8b3c
plbalmeida <plbalmeida@usp.br>
2025-06-27 15:55:27 -0300

    test(infra/s3/backend.tf): teste sem pasar a role

--- infra/s3/backend.tf
+++ infra/s3/backend.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "s3" {
     region         = "us-east-1"
-    role_arn       = "arn:aws:iam::626957573797:role/LabRole"
     bucket         = "terraform-backend-s3-ipea-eia366-pbrent366"
     key            = "terraform.tfstate"
     dynamodb_table = "terraform-backend-lock-table-s3-ipea-eia366-pbrent366"
